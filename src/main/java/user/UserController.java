d62c2a130a2a066224ea50e62ac8e4fd4b0c4f30
sead-afk <61579298+sead-afk@users.noreply.github.com>
2023-11-23 14:34:38 +0100

    Initial commit

---
+++ src/main/java/user/UserController.java
@@ -0,0 +1,42 @@
+package user;
+
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "api/v1/user")
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public List<User> getUser(){
+        return userService.getUsers();
+    }
+
+    @PostMapping
+    public void registerNewUser(@RequestBody User user){
+        userService.addNewUser(user);
+    }
+
+    @DeleteMapping(path = {"userId"})
+    public void deleteUser(@PathVariable("userId") Long userId){
+        userService.deleteUser(userId);
+    }
+
+    @PutMapping(path = "{userId}")
+    public void updateUser(
+            @PathVariable("{userId}") Long userId,
+            @RequestParam(required = false) String username,
+            @RequestParam(required = false) String email){
+        userService.updateUser(userId, username, email);
+    }
+}
---
+++ src/test/java/com/RideAroundTheWorld/SpringBootTravelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RideAroundTheWorld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTravelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
