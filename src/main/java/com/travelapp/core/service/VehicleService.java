588401c199826aeb531d66d02fcb481d46a0256b
sead-afk <61579298+sead-afk@users.noreply.github.com>
2023-11-27 13:13:12 +0100

    Vehicle bean addition v1

--- src/main/java/com/travelapp/core/service/VehicleService.java
+++ src/main/java/com/travelapp/core/service/VehicleService.java
@@ -2,8 +2,12 @@ package com.travelapp.core.service;
 
 import com.travelapp.core.model.Destination;
 import com.travelapp.core.model.Vehicle;
+import com.travelapp.core.model.enums.DestinationType;
+import com.travelapp.core.model.enums.VehicleType;
 import com.travelapp.core.repository.VehicleRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class VehicleService {
@@ -14,6 +18,14 @@ public class VehicleService {
     }
 
     public Vehicle addVehicle(Vehicle vehicle) {
-        return VehicleRepository.save(vehicle);
+        return vehicleRepository.save(vehicle);
+    }
+
+    public List<Vehicle> getAll() {
+        return vehicleRepository.findAll();
+    }
+
+    public List<Vehicle> filter(VehicleType vehicleType, String name) {
+        return vehicleRepository.findAllByVehicleTypeOrNameLike(vehicleType, name);
     }
 }
