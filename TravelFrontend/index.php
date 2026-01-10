67a50929c04c005e8dbc220d8e13af1fa590eb8b
Sead Gačanović <61579298+sead-afk@users.noreply.github.com>
2026-01-10 16:25:41 +0100

    Delete TravelFrontend directory

--- TravelFrontend/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-require 'vendor/autoload.php'; //run autoloader
-
-Flight::route('/', function(){ //define route and define function to handle request
-    echo 'Hello worldsssss!';
-});
-
-Flight::start(); //start FlightPHP
-?>
