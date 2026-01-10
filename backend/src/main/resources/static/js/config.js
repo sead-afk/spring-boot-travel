330f7757b0638ed356b3acd2bd9c7a9d56c56700
sead-afk <sead.gacanovic@stu.ibu.edu.ba>
2026-01-10 23:39:30 +0100

    module import and export adjustments

--- backend/src/main/resources/static/js/config.js
+++ backend/src/main/resources/static/js/config.js
@@ -1,4 +1,4 @@
-const API_BASE_URL =
+export const API_BASE_URL =
   window.location.hostname === "localhost"
     ? "http://localhost:8080"
     : "https://spring-boot-travel-production.up.railway.app";
