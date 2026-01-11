afe18cd13131337d5404a9e1326844295dc0151e
sead-afk <sead.gacanovic@stu.ibu.edu.ba>
2026-01-11 16:26:59 +0100

    deleting frontend parts,decoupling

--- backend/src/main/resources/static/js/config.js
+++
@@ -1,4 +0,0 @@
-export const API_BASE_URL =
-  window.location.hostname === "localhost"
-    ? "http://localhost:8080"
-    : "https://spring-boot-travel-production.up.railway.app";
--- backend/src/main/resources/static/utils/assets/js/constants.js
+++
@@ -1,3 +0,0 @@
-var Constants = {
-    //${API_BASE_URL}:
-}
